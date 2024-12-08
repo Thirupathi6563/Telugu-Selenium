f2042e8a624f2b23b5de06f888899da0e8d692f2
thiru <thirupathi6563@gmail.com>
2024-12-09 00:28:31 +0530

    title commit

--- SeleniumTelugu/src/test/java/com/teluguselenium/SeleniumTelugu/GetTitle.java
+++ SeleniumTelugu/src/test/java/com/teluguselenium/SeleniumTelugu/GetTitle.java
@@ -9,6 +9,7 @@ public class GetTitle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver =new ChromeDriver();
 		driver.get("https://www.google.com/");
